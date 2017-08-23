46bcb47a201f190759f445c9752c1fd6f1fed297
Sunil Kumar <sunilk@360logica.com>
2017-08-23 18:44:03 +0530

    Assertion Class Added

--- TestExample/src/test/java/com/maventest/TestExample/NavigationExamples.java
+++ TestExample/src/test/java/com/maventest/TestExample/NavigationExamples.java
@@ -17,8 +17,9 @@ public class NavigationExamples {
 	{
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\sunilk\\workspace\\TestExample\\Drivers\\geckodriver.exe");
 		driver= new FirefoxDriver();
+		//driver.manage().window().maximize();
 		driver.navigate().to("http://www.uftHelp.com");
-		driver.manage().window().maximize();
+			
 		System.out.println("Browser Navigated to **http://www.uftHelp.com**");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//div[@class='readmore']/a[@href='http://www.ufthelp.com/2017/08/how-to-install-ie-browser-on-mac-os.html']")).click();
